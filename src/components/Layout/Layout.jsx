2ed38fe8cca9603fcaa81f6926d68c5ff0606949
Juan Pablo Ortiz <janpoloy@gmail.com>
2019-02-19 06:57:39 -0600

    add a loading on Gatekeeper interaction

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Spin } from 'antd';
 import Header from '../Header/Header';
+import STATUS from '../../constants/status';
 
 const { Content, Footer } = Layout;
 
@@ -9,16 +10,22 @@ const styles = {
 }
 export default props => (
   <Layout style={ styles.layout } className="layout">
-    <Header />
-    <Content style={{ padding: '0 50px' }}>
-      <div style={{ padding: 24, minHeight: '80vh' }}>
-        { props.children }
+    { props.status === STATUS.LOADING ? (
+      <div><Spin />Waiting for Gatekeeper server...</div>
+    ) : (
+      <div>
+        <Header />
+        <Content style={{ padding: '0 50px' }}>
+          <div style={{ padding: 24, minHeight: '80vh' }}>
+            { props.children }
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          <a href="http://juanpablo.xyz">juanpablo.xyz </a>
+          | 2019 | 
+          <a href="https://twitter.com/janpoloy"> @janpoloy</a>
+        </Footer>
       </div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>
-      <a href="http://juanpablo.xyz">juanpablo.xyz </a>
-      | 2019 | 
-      <a href="https://twitter.com/janpoloy"> @janpoloy</a>
-    </Footer>
+    )}
   </Layout>
 );
